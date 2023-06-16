97dd2c200559a33adf0d8f1d1db003f4400e52f7
Fernando Rivero <frivero@itadmin.com.mx>
2023-06-16 12:32:28 -0500

    Update __manifest__.py, res_company.py, and res_company_view.xml

--- l10n_mx_edi_mx_pac/__manifest__.py
+++ l10n_mx_edi_mx_pac/__manifest__.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 {
     'name':         'IT Admin PAC',
-    'version': '    15.01',
+    'version': '    15.02',
     'description':  ''' 
                     Agrega a IT Admin como un PAC adicional para el timbrado de documentos.
                     ''',
@@ -9,7 +9,7 @@
     'author':       'IT Admin',
     'website':      'www.itadmin.com.mx',
     'depends':      ['account', 'l10n_mx_edi'],
-    'data':         [],
+    'data':         ['views/res_company_view.xml',],
 
     'application':  False,
     'installable':  True,
